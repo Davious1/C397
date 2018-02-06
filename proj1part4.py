3ac15d717d654740913ffba6058a32a3c67c2fc9
Davious1 <jarmfiel@ualberta.ca>
2018-02-06 11:56:05 -0700

    Update proj1part4.py

--- proj1part4.py
+++ proj1part4.py
@@ -1,1 +1,22 @@
+import sys
+import re
+import os
 
+takein = sys.stdin
+
+def terms():
+	pat = r'<(article|inproceedings) key=\"([\w\/-]+)\">(.*?)</\1>'
+	auth = r'<author>(.*?)</author>'
+	titl = r'<title>(.*?)</title>'
+	jour = r'<(journal|booktitle|publisher)>(.*?)</\1>'
+	year = r'<year>(.*?)</year>'
+	try:
+		os.remove('terms.txt')
+		os.remove('years.txt')
+		os.remove('recs.txt')
+	except:
+		pass
+
+	file=open('terms.txt','w')
+	f=open('years.txt','w')
+	fi=open('recs.txt','w')
